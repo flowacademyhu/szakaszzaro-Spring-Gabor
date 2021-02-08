edfee5b20cd7ec9fc48e369e60cf28dcc03804bf
BigG <gabszon002@gmail.com>
2021-02-08 10:28:59 +0100

    Geting ready for szakaszzaro test

---
+++ src/test/java/com/vizsgafeladat/vizsgafeladat/VizsgafeladatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vizsgafeladat.vizsgafeladat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VizsgafeladatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
